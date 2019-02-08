27afffb24dd0a550a00be151bbdf28a680a33000
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-08 12:48:19 -0500

    Updated from Brython Server: 2/8/2019 12:48:20 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
 Author: Sean Healey
-Credit: None
+Credit: Tutorials
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
